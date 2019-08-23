8f28621c5d41a789c9a80f52cdf277ffb08588c0
Remeto <remetojohn@gmail.com>
2019-08-22 18:57:29 -0700

    finished html routing

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,6 @@
+const path = require('require');
+
+module.exports = function apiRoutes(app){
+    
+
+}
---
+++ server.js
@@ -0,0 +1,20 @@
+//set up dependancies
+const express = require('express');
+
+
+
+//set-up for express
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require('./app/routing/htmlRoutes.js')(app);
+
+app.listen(PORT, () => {
+    console.log(`Server running on PORT ${PORT}`);
+});
